0e60485362b49b3fd9ccd18b8355142dfb55a34b
J. Maurice <j@wiz.biz>
2013-01-27 21:23:52 +0900

    Use correct document.location.protocol in QR code URL

--- src/_js/inc/ext/qrcode.js
+++ src/_js/inc/ext/qrcode.js
@@ -34,7 +34,7 @@ modulejs.define('ext/qrcode', ['_', '$', 'modernizr', 'core/settings', 'core/eve
 					width: settings.size,
 					height: settings.size,
 					color: '#333',
-					text: 'http://' + document.domain + item.absHref
+					text: document.location.protocol + '//' + document.domain + item.absHref
 				});
 			});
 		},
